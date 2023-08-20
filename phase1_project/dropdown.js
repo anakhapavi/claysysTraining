b9d61e4eb4b750a8c22e3f448192f5db7256aa9c
anakhapavi <71835996+anakhapavi@users.noreply.github.com>
2023-08-20 22:00:48 +0530

    Update dropdown.js

--- phase1_project/dropdown.js
+++ phase1_project/dropdown.js
@@ -1,26 +1,31 @@
+ document.addEventListener('DOMContentLoaded', function() {
 
-const stateDropdown = document.getElementById('state');
-const cityDropdown = document.getElementById('city');
+  const stateDropdown = document.getElementById('state');
+  const cityDropdown = document.getElementById('city');
 
-// Define city options for each state
-const cityOptions = {
-    AndhraPradesh: ['Vishakapattanam', 'Vijayawada', 'Tirupati'],
-    Goa: ['Vasco','Ponda','Panaji'],
-    Karnataka: ['Bangalore', 'Udupi', 'Tumkur'],
-    Kerala: ['Kochi','Trivandrum','Kottayam'],
-    TamilNadu: ['Cheannai','Madurai','Coimbatore']
-};
+  // Define city options for each state
+    const cityOptions = {
+      AndhraPradesh: ['Vishakapattanam', 'Vijayawada', 'Tirupati'],
+      Goa: ['Vasco', 'Ponda', 'Panaji'],
+      Karnataka: ['Bangalore', 'Udupi', 'Tumkur'],
+      Kerala: ['Kochi', 'Trivandrum', 'Kottayam'],
+      TamilNadu: ['Chennai', 'Madurai', 'Coimbatore']
+    };
 
-stateDropdown.addEventListener('change', function() {
-    const selectedState = stateDropdown.value;
-    const cities = cityOptions[selectedState] || [];
+    stateDropdown.addEventListener('change', function() {
+      const selectedState = stateDropdown.value;
+      const cities = cityOptions[selectedState] || [];
 
-    cityDropdown.innerHTML = '<option value="none">Select a city</option>';
+      cityDropdown.innerHTML = '<option value="none">Select a city</option>';
 
-    cities.forEach(city => {
-      const option = document.createElement('option');
-      option.value = city.toLowerCase().replace(' ', '');
-      option.textContent = city;
-      cityDropdown.appendChild(option);
+      cities.forEach(city => {
+          const option = document.createElement('option');
+          option.value = city.toLowerCase().replace(' ', '');
+          option.textContent = city;
+          cityDropdown.appendChild(option);
+      });
+
+      // Show or hide the city dropdown based on the selected state
+      cityDropdown.style.display = cities.length > 0 ? 'block' : 'none';
     });
   });
