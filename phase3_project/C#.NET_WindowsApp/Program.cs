ce7f928f555a0f91de1f19ab7e1771eb41fb6851
anakhapavi <71835996+anakhapavi@users.noreply.github.com>
2023-08-30 21:54:58 +0530

    Update Program.cs

--- phase3_project/C#.NET_WindowsApp/Program.cs
+++ phase3_project/C#.NET_WindowsApp/Program.cs
@@ -1,17 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Claysys_WinFormsApp
 {
-    internal static class Program
+    public partial class index : Form
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public index()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new login_new());
+            InitializeComponent();
+        }
+        int i;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i <= 100)
+            {
+                progressBar1.Value = i;
+                label1.Text = i.ToString() + "%";
+                i += 5;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                this.Hide();
+                Home obj = new Home();
+                obj.ShowDialog();
+            }
         }
     }
 }
