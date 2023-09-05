a090a3282843cee6611bccc571a0ab6d33b37df1
anakhapavi <71835996+anakhapavi@users.noreply.github.com>
2023-09-05 23:24:25 +0530

    Update ItemController.cs

--- phase_4_project/ASP.NET MVC APPLICATION/ItemController.cs
+++ phase_4_project/ASP.NET MVC APPLICATION/ItemController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using MVC_ProductApplication.DAL;
@@ -14,7 +15,10 @@ namespace MVC_ProductApplication.Controllers
         item_DAL obj = new item_DAL();
 
 
-        // GET: Item
+        /// <summary>
+        /// GET: Item
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             var itemList = obj.GetAllItems();
@@ -25,102 +29,130 @@ namespace MVC_ProductApplication.Controllers
             return View(itemList);
         }
 
-        // GET: Item/Details/5
+        /// <summary>
+        /// GET: Item/Details/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Item/Create
+
+        /// <summary>
+        /// GET: Item/Create
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Item/Create
+        /// <summary>
+        /// POST: Item/Create
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult Create(Item item,HttpPostedFileBase image)
+        public ActionResult Create(Item item)
         {
-            try
-            {
-                // TODO: Add insert logic here
-                bool IsInserted = false;
-                if (ModelState.IsValid && image != null && image.ContentLength > 0)
+            bool IsInserted = false;
+            if (ModelState.IsValid)
+            { 
+                IsInserted = obj.InsertItems(item);
+                if (IsInserted)
                 {
-                    string fileName = Path.GetFileName(image.FileName);
-                    string uniqueFileName = Guid.NewGuid().ToString()+"_"+fileName;
-                    string filePath = Path.Combine(Server.MapPath("~/Images/"),uniqueFileName);
-                    image.SaveAs(filePath);
-
-                    item.image = uniqueFileName;
-                    bool isInserted = item_DAL.InsertItems(item, image);
-
-
-                    if (IsInserted)
-                    {
-                        TempData["SucessMessage"] = "Item details are saved sucessfully";
-                    }
-                    else
-                    {
-                        TempData["ErrorMessage"] = "Unable to save item details";
-                    }
+                    ///TempData["SuccessMessage"] = "Saved Sucessfully";
                 }
-                else
-                {
-                    ModelState.AddModelError("", "Please provide a valid image.");
-                    return View(item); // Return to the create view with validation errors
+                else {
+                    ///TempData["ErrorMessage"] = " save";
                 }
                 return RedirectToAction("Index");
             }
-            catch(Exception ex)
-            {
-                TempData["ErrorMessage"] = ex.Message;
-                return View();
-            }
+            return View();
         }
 
-        // GET: Item/Edit/5
+
+
+        /// <summary>
+        /// GET: Item/Edit/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Edit(int id)
         {
+            var item = obj.GetProductsById(id).FirstOrDefault();
+            if (item == null)
+            {
+                TempData["infoMessage"] = "currently no data is available with id"+id.ToString();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        /// <summary>
+        /// POST: Item/Edit/5
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpPost,ActionName("Edit")]
+        public ActionResult Update(Item item)
+        {
+            if (ModelState.IsValid)
+            { 
+                bool IsUpdated = obj.UpdateItem(item);
+                if (IsUpdated)
+                { 
+                
+                }
+                else{
+                
+                }
+                return RedirectToAction("Index");
+            }
             return View();
         }
 
-        // POST: Item/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        /// <summary>
+        /// GET: Item/Delete/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
 
-        // GET: Item/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
-        }
-
-        // POST: Item/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
+            var item = obj.GetProductsById(id).FirstOrDefault();
+            if (item == null)
             {
-                // TODO: Add delete logic here
+                TempData["infoMessage"] = "currently no data is available with id" + id.ToString();
+                return RedirectToAction("Index");
+
+            }
+            return View(item);
+        }
+        /// <summary>
+        /// POST: Item/Delete/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            /// Call the DeleteItem method to delete the item by its ID
+            obj.DeleteItem(id);
+
+            if (ModelState.IsValid)
+            {
+                
 
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
+
+
     }
 }
