74f35b40e34d89744b7dcb41bf7b6972e8313aae
anakhapavi <71835996+anakhapavi@users.noreply.github.com>
2023-09-07 13:12:00 +0530

    Update image_DAL.cs

--- phase_4_project/ASP.NET MVC APPLICATION/image_DAL.cs
+++ phase_4_project/ASP.NET MVC APPLICATION/image_DAL.cs
@@ -14,7 +14,36 @@ namespace MVC_ProductApplication.DAL
         string connectionString = ConfigurationManager.ConnectionStrings["shopConnectionString"].ToString();
 
 
-        
+         public List<Image> GetImages()
+ {
+     List<Image> imgList = new List<Image>();
+
+     using (SqlConnection con = new SqlConnection(connectionString))
+     {
+         SqlCommand cmd = con.CreateCommand();
+         cmd.CommandType = CommandType.StoredProcedure;
+         cmd.CommandText = "sp_ViewImage";
+
+         con.Open();
+         SqlDataReader rdr = cmd.ExecuteReader();
+
+         while (rdr.Read())
+         {
+             string imgName = rdr["imgname"].ToString();
+             byte[] imgData = rdr["imgpath"] as byte[];
+
+             imgList.Add(new Image
+             {
+                 imgname = imgName,
+                 imgpath = imgData
+             });
+         }
+         con.Close();
+     }
+
+     return imgList;
+
+ }
 
         public bool InsertImage(Image image)
         {
